f6a5a5f3d40c2b4762543a223cd053b1c3e61718
EngineerSebas <70970230+EngineerSebas@users.noreply.github.com>
2022-07-15 11:46:52 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>EasyVet</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Bienvenidos a EasyVet!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Empezamos a hacer codigo<code>pages/index.js</code>
         </p>
       </main>
 
